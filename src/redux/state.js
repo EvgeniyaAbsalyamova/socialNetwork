5ee2953704600eca4e390a237ed81d35a813abc8
Evgeniya Absalyamova <Timoshokevgeniya@mail.ru>
2022-09-01 10:58:00 +0300

    39les.Action type

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
 // import { getSpaceUntilMaxLength } from "@testing-library/user-event/dist/utils";
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
 let store = {
   _state: {
@@ -35,7 +37,7 @@ let store = {
   },
 
   dispatch(action) {
-    if (action.type === "ADD-POST") {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 3,
         message: this._state.profilePage.newPostText,
@@ -44,7 +46,7 @@ let store = {
       this._state.profilePage.posts.push(newPost);
       this._state.profilePage.newPostText = "";
       this._callSubscriber(this._state);
-    } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
       this._state.profilePage.newPostText = action.newText;
       this._callSubscriber(this._state);
     }
@@ -53,15 +55,20 @@ let store = {
 
 export const addPostActionCreator = () => {
   return {
-    type: "ADD-POST",
+    type: ADD_POST,
   };
 };
+// export const addPostActionCreator = () => ({ type: ADD_POST });
+
 export const updateNewPostTextActionCreator = (text) => {
   return {
-    type: "UPDATE-NEW-POST-TEXT",
+    type: UPDATE_NEW_POST_TEXT,
     newText: text,
   };
 };
+
+// export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT,
+//     newText: text});
 
 window.store = store;
 export default store;
