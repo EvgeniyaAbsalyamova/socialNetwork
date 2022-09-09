77e78536f903fac64e7ff384c6d4ac19171dcd3a
Evgeniya Absalyamova <Timoshokevgeniya@mail.ru>
2022-09-09 14:43:58 +0300

    42les.Redux

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,21 @@
 const UPDATE_NEW_MESSAGE_BODY = "UPDATE_NEW_MESSAGE_BODY";
 const SEND_MESSAGE = "SEND_MESSAGE";
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+  dialogs: [
+    { id: 1, name: "Evgeniya" },
+    { id: 2, name: "Eva" },
+    { id: 3, name: "Zhenya" },
+  ],
+  messages: [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How are you" },
+    { id: 3, message: "I love you" },
+  ],
+  newMessageBody: "",
+};
+
+const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_NEW_MESSAGE_BODY:
       state.newMessageBody = action.body;
